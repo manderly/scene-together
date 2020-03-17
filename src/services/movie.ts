fbbd0775ad3abb8039f8ee6648c03cffae94e40d
Mandi <mandi018@gmail.com>
2020-03-17 09:44:46 -0500

    More complicated example query. Changed ? to & for api key

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -1,6 +1,6 @@
 import { getAPI } from "./api"
 
 export const findMovie = async () => {
-    const result = await getAPI('3/movie/550');
+    const result = await getAPI('3/discover/movie?with_people=287,819&sort_by=vote_average.desc');
     return result.data;
 }
